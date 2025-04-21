ba5f0f488e17318089bb50aa6e071d6fd4904bbf
Ajongafac Zinkeng <ajongafaczinkeng@gmail.com>
2025-04-21 16:54:11 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,42 +3,62 @@ import {FaWhatsapp, FaTwitter, FaInstagram, FaMailBulk, FaFacebook} from 'react-
 
 function Footer() {
   return (
+    <div>
     <div className="footer text-white justify-between gap-16 bg-gray-900 px-5 lg:px-28  ">
       <p>Logo</p>
-      <div className="footer-1 items-center align-middle justify-center flex">
-        <div className="pages flex gap-16 font-medium align-middle">
-          <div className="pages-1">
+      <div className="footer-1 items-center align-middle justify-center gap-30 flex">
+        <div className="pages gap-16 font-medium align-middle">
+          <div className="pages-1 flex gap-16">
             <p>Home</p>
             <p>About Me</p>
             <p>Services</p>
           </div>
 
-          <div className="pages-2">
+          <div className="pages-2 mt-7 flex gap-16">
             <p>Resume</p>
             <p>Portfolio</p>
             <p>Blog</p>
           </div>
-        </div>
 
-        <div className="message border-l-2 border-pink-400">
-            <form action="" className="ml-4 flex flex-col">Email <br/>
-                <input type="E-mail" className=" mt-4 h-8 bg-gray-600 rounded-sm " placeholder="Please enter your email" /> <br/>
-                <input type="text" className=" mt-4 h-15 rounded-sm bg-gray-600 " placeholder="Leave a message..." />
-                <button className="btn text-center border-black font-bold bg-pink-400 text-black shadow-lg rounded-lg border-2 h-7 w-20">
-              Submit
-            </button>
-            </form>
-        </div>
-      </div>
-
-      <div className="flex justify-center mt-5">
+          <div className="justify-center mt-20" >
+        <p>SOCIAL MEDIA</p>
+      <div className="flex">
       <FaWhatsapp size={23} color="#ffff"  className="m-2"/>
       <FaTwitter size={23} color="#ffff" className="m-2"/>
       <FaFacebook size={23} color="#ffff" className="m-2"/>
       <FaInstagram size={23} color="#ffff" className="m-2"/>
       <FaMailBulk size={23} color="#ffff" className="m-2"/>
           </div>
-          <p className="text-center bg-black mt-5" >ALL RIGHTS RESERVED @Zinkeng @2025</p>
+          </div>
+        </div>
+
+        <div className="message">
+            <form action="" className="ml-4 flex flex-col">
+            <p className="font-bold">Leave a Message...</p>
+                <input type="E-mail" className=" mt-4 h-8 bg-gray-600 rounded-sm w-80 " placeholder="Please enter your email" /> <br/>
+                <input type="text" className="h-20 rounded-sm bg-gray-600 " placeholder="How can we help you?" />
+                <button className="btn text-center font-bold bg-pink-400 text-white shadow-lg rounded-lg h-8 w-25 mt-3 ">
+              Submit
+            </button>
+            </form>
+        </div>
+      </div>
+
+          
+    </div>
+    <div className="flex justify-between bg-black w-full px-28">
+            <div>
+          <p className="textt-center text-gray-700 mt-2" > Copyright @2025 AZM, All Rights Reserved.</p>
+          </div>
+          <div className="flex gap-16">
+          <div>
+          <p className="text-center text-gray-700 bg-black mt-2" > Privacy Policy</p>
+          </div>
+          <div>
+          <p className="text-center text-gray-700 bg-black mt-2" > Terms of Use</p>
+          </div>
+          </div>
+          </div>
     </div>
   );
 }
