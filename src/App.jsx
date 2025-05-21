1cab81055a87d10c6a8216a4fa5341fc2ce0680e
Ajongafac Zinkeng <ajongafaczinkeng@gmail.com>
2025-05-21 23:36:52 +0100

    Update on pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Home from './Pages/Home';
 import Portfolio from './Pages/Portfolio';
+import Contact from './components/Contact';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 
 
 function App() {
   return (<>
-  <Home />
+
+  <Router>
+      <nav>
+        {/* Navigation Links */}
+        {/* <Link to="/home">Home</Link>
+        <Link to="/contact">Contact</Link> */}
+      </nav>
+
+      <Routes>
+        {/* Page Routes */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        
+      </Routes>
+    </Router>
   {/* <Portfolio /> */}
   </>)
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Contact (){
+    return (<>
+
+    <h1>I am the contact page</h1>
+
+    </>
+
+    )
+}
+
+
+export default Contact;
